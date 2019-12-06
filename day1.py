406784d6413ae88ab3bc3df87ac6950af18d3fe4
OpesMentis <planchot.antoine@gmail.com>
2019-12-06 22:35:18 +0100

    Ajout des parties 1 et 2

--- day1.py
+++ day1.py
@@ -1,9 +1,12 @@
 d=open('data/input1.txt', 'r')
+e=d.readlines()[:-1]
+print(sum(int(m)//3-2 for m in e))
+
 def f(m,s):
     if m>0:
         u=max(0,m//3-2)
         return f(u,s+u)
     return s
-l=map(int, d.readlines()[:-1])
+l=map(int, e)
 print(sum(f(m,0) for m in l))
 d.close()
--- day2.py
+++ day2.py
@@ -1,9 +1,39 @@
+d=open('data/input2.txt', 'r')
+s=d.read().split(',')
+l=[int(i)for i in s]
+l[1:3]=[12,2]
+i=0
+p=l[0]
+while p<3:
+    a=l[l[i+1]]
+    b=l[l[i+2]]
+    c=l[i+3]
+    if p==1:
+        l[c]=a+b
+    elif p==2:
+        l[c]=a*b
+    i+=4
+    p=l[i]
+
+print(l[0])
+
 def f(l):
-  i,p=0,l[0]
-  while p<3:a,b,c=l[i+1:i+4];l[c],i=[l[a]+l[b],l[a]*l[b]][p-1],i+4;p=l[i]
-  return l[0]
-i=list(map(int,open('data/input2.txt').read().split(',')))
+    i,p=0,l[0]
+    while p<3:
+        a,b,c=l[i+1:i+4]
+        l[c]=[l[a]+l[b],l[a]*l[b]][p-1]
+        i+=4
+        p=l[i]
+    return l[0]
+
+i=list(map(int,s))
 h=100
 for x in range(h*h):
-  n,v=x//h,x%h;l=i[::];l[1:3]=n,v
-  if f(l)==19690720:print(h*n+v)
+    n,v=x//h,x%h
+    l=i[::]
+    l[1:3]=n,v
+    if f(l)==19690720:
+        print(h*n+v)
+        break
+
+d.close()
