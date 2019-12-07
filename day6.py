d5ea013b4e2f21fbf4efbc33df24fca89bf061e9
OpesMentis <planchot.antoine@gmail.com>
2019-12-07 17:50:41 +0100

    Problem 6, part 2.

--- day6.py
+++ day6.py
@@ -10,11 +10,25 @@ for orb in orbs:
     d[c2] = c1
 
 s = 0
-
 for p in d:
     curr_p = p
     while curr_p in d:
         curr_p = d[curr_p]
         s += 1
-
 print(s)
+
+trajet_you = []
+curr = 'YOU'
+while True:
+    if curr not in d: break
+    curr = d[curr]
+    trajet_you += [curr]
+
+i = 0
+curr = 'SAN'
+while True:
+    i += 1
+    curr = d[curr]
+    if curr in trajet_you:break
+
+print(trajet_you.index(curr) + i - 1)        
